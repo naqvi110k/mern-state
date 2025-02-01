6cee6843b4d517d2f0eb4f06398e400610299925
Syed Mohsin Abbas Naqvi <naqvisyed9221@gmail.com>
2025-02-01 17:04:03 +0500

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,37 +2,34 @@ import React, { useState } from 'react'
 import {Link, useNavigate} from "react-router-dom"
 import axios from "axios"
 import toast from "react-hot-toast"
+import { useDispatch, useSelector } from 'react-redux'
+import {signInStart, signInSuccess, signInFailure} from "../redux/user/userSlice"
 
 const SignIn = () => {
-  const navigate = useNavigate()
-  const [formData, setFormData] =useState({})
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const handleChange = (e) =>{
-    setFormData({...formData, [e.target.id]: e.target.value})
-  }
-  const handleSubmit = async(e) => {
-    e.preventDefault()
-   try {
-    setLoading(true)
-    //Send data to server
-    await axios.post("/api/auth/signin", formData)
-    .then(response =>{
-      if (response.data){
-        setLoading(false)
-        setError(null)
-        navigate("/")
-        toast.success("Signed in successfully!");
-      }
-    })
-    } catch (error) {
-         if (error.response) {
-          toast.error("Error: " + error.response.data.message);
-          setLoading(false)
-        }
-      }
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
-  }
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch(signInStart());
+    try {
+      const response = await axios.post("/api/auth/signin", formData);
+      dispatch(signInSuccess(response.data)); // Pass user data to Redux
+      navigate("/");
+      toast.success("Signed in successfully!");
+    } catch (error) {
+      if (error.response) {
+        toast.error("Error: " + error.response.data.message);
+        dispatch(signInFailure(error.response.data.message));
+      }
+    }
+  };
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
