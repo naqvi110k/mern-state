637533a17dcbb5a1692653bc5d2e732da749f3d6
Syed Mohsin Abbas Naqvi <naqvisyed9221@gmail.com>
2025-02-01 15:58:42 +0500

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import bcrypt from "bcrypt";
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken"
+
 export const  signup =  async(req,res,next) =>{
     
 try {
@@ -43,5 +45,46 @@ try {
     
 } catch (error) {
     next(error);
+    }
+}
+
+export const signin = async (req, res, next) => {
+    const {email , password} = req.body;
+
+    try {
+        // Validate request data
+        if(!email ||!password){
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+
+        // Check if email exists
+        const user = await User.findOne({ email });
+        if(!user){
+            return res.status(404).json({ message: 'User not found' });
+        }   
+
+        // Check if password matches
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        // Generate JWT token
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+        res.cookie("acess-token", token, {
+            httpOnly: true
+
+        }) 
+         const {password: pass , ...rest} = user._doc
+        res.json({ message: 'User authenticated successfully', rest});
+
+
+
+
+
+        
+        
+    } catch (error) {
+        next(error);
     }
 }
