af59e4179efd6150b798c57dee151062c09da85b
Syed Mohsin Abbas Naqvi <naqvisyed9221@gmail.com>
2025-02-01 20:43:03 +0500

    complete profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+  const {currentUser} = useSelector((state) => state.user)
   return (
-    <div>Profile</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl  font font-semibold text-center my-7'>Profile</h1>
+
+      <form  className='flex flex-col gap-4'>
+        <img src={currentUser.avatar} alt="profile"  className=' mt-2 rounded-full h-24 w-24 object-cover 
+        self-center cursor-pointer'/>
+        <input type="text" placeholder='username' id='username'
+        className='border p-3 rounded-lg '/>
+        <input type="email" placeholder='email' id='email'
+        className='border p-3 rounded-lg '/>
+        <input type="password" placeholder='password' id='password' 
+        className='border p-3 rounded-lg'/>
+        <button className='bg-slate-700 text-white p-3 rounded-lg
+        uppercase hover:opacity-95 disabled:opacity-80'> Update</button>
+
+      </form>
+      <div className='flex justify-between mt-5'>
+        <span className='text-red-700 cursor-pointer'> Delete account </span>
+        <span className='text-red-700 cursor-pointer'> Sign out </span>
+      </div>
+  
+    </div>
+    
   )
 }
 
