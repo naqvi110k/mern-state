1301edec602a85e56fb737a6e025584ce43e4881
Syed Mohsin Abbas Naqvi <naqvisyed9221@gmail.com>
2025-02-03 15:24:34 +0500

    create listing route

---
+++ api/routes/listing.route.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { createListing } from '../controllers/listing.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+
+
+const router = express.Router();
+
+
+router.get("/test", ()=>{
+    console.log('Hello from the API endpoint!');
+})
+
+
+router.post("/create", verifyToken, createListing)
+
+export default router;
