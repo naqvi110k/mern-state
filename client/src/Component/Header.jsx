5fd46b6ce87b04a27ae2af76100d8d6077673137
Syed Mohsin Abbas Naqvi <naqvisyed9221@gmail.com>
2025-02-08 18:50:05 +0500

    compelte project

--- client/src/Component/Header.jsx
+++ client/src/Component/Header.jsx
@@ -26,8 +26,8 @@ export default function Header() {
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
-          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
+          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap gap-1'>
+            <span className='text-slate-500'>SMAN</span>
             <span className='text-slate-700'>Estate</span>
           </h1>
         </Link>
